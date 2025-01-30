c78715ebf5db20b2645bde852c69dd14413e95aa
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:42:24 +0530

    Add files via upload

---
+++ Leapyear.java
@@ -0,0 +1,10 @@
+public class Leapyear {
+    public static void main(String[]args){
+        int year=2024;
+        if(year%4==0){
+            System.out.println(year+"is a leap year");
+
+        }
+    }
+    
+}
